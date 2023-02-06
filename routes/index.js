8d9e7f718fceaf116293dc98424c68bfcbd4ab5f
Mikhail Pestov <mnpestov@gmail.com>
2023-02-06 14:03:00 +0300

    autotests github

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
+const httpConstants = require('http2').constants;
 const express = require('express');
 const { userRoutes } = require('./users');
 const { cardRoutes } = require('./cards');
@@ -6,5 +7,8 @@ const routes = express.Router();
 
 routes.use('/users', userRoutes);
 routes.use('/cards', cardRoutes);
+routes.use('*', (req, res) => {
+  res.status(httpConstants.HTTP_STATUS_NOT_FOUND).send({ message: 'Маршрут не найден' });
+});
 
 exports.routes = routes;
