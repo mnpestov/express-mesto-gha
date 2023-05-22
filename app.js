b8ae1ac8fe4c5d7b67804d40ec13e171ffd310d2
Mikhail Pestov <mnpestov@gmail.com>
2023-05-22 19:50:46 +0300

    автотесты

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.post('/signup', express.json(), celebrate({
     password: Joi.string().required(),
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string(),
+    avatar: Joi.string().pattern(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/),
   }),
 }), createUser);
 app.use(auth);
