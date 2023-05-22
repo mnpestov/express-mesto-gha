4190eec08f9a177f3a55f7ab2dca19ec2415b934
Mikhail Pestov <mnpestov@gmail.com>
2023-05-22 19:30:37 +0300

    переписал аус как в теории

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,26 +1,37 @@
-// const httpConstants = require('http2').constants;
 const jwt = require('jsonwebtoken');
 
 const JWT_SOLT = 'wotj21ds0f7!hjhjh^';
 
+// module.exports = (req, res, next) => {
+//   let payload;
+//   try {
+//     const token = req.headers.authorization;
+//     if (!token) {
+//       throw new Error('unauthorized');
+//     }
+//     payload = jwt.verify(token, JWT_SOLT);
+//   } catch (err) {
+//     next(err);
+//   }
+//   req.user = payload; // записываем пейлоуд в объект запроса
+//   next(); // пропускаем запрос дальше
+// };
 module.exports = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    throw new Error('unauthorized');
+  }
+
+  const token = authorization.replace('Bearer ', '');
   let payload;
   try {
-    const token = req.headers.authorization;
-    if (!token) {
-      throw new Error('unauthorized');
-    }
     payload = jwt.verify(token, JWT_SOLT);
   } catch (err) {
     next(err);
-    // if (err.message === 'unauthorized') {
-    //   res.status(httpConstants.HTTP_STATUS_UNAUTHORIZED)
-    //     .send({ message: 'Необходимо авторизоваться', ...err });
-    // } else {
-    //   res.status(httpConstants.HTTP_STATUS_INTERNAL_SERVER_ERROR)
-    //     .send({ message: 'Ошибка на сервере', ...err });
-    // }
   }
+
   req.user = payload; // записываем пейлоуд в объект запроса
+
   next(); // пропускаем запрос дальше
 };
